1e1ea3a30d2cf6a774dc7d4cadebd58eac4f5acb
Robert Lin <robertlin1@gmail.com>
2019-04-24 12:11:09 -0700

    test: add test for observable

---
+++ test/test_test.go
@@ -0,0 +1,18 @@
+package test
+
+import (
+	"testing"
+
+	"go.uber.org/zap/zapcore"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewObservable(t *testing.T) {
+	logger, out := NewObservable()
+	logger.Info("hi")
+	require.True(t, len(out.All()) > 0, "log should have been captured")
+	assert.Equal(t, out.All()[0].Message, "hi")
+	assert.Equal(t, out.All()[0].Level, zapcore.InfoLevel)
+}
