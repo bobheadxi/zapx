ae07261cd3fb172ddabf465e4505365819bf50ce
Robert Lin <robertlin1@gmail.com>
2019-04-25 14:08:46 -0700

    zapx: move options to separate file

--- zapx.go
+++ zapx.go
@@ -1,7 +1,6 @@
 package zapx
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -9,28 +8,6 @@ import (
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
-
-// Option denotes an option for the zapx constructor
-type Option func(*zap.Config) error
-
-// OnlyToFile removes all other outputs and only writes to file
-func OnlyToFile() Option {
-	return func(cfg *zap.Config) error {
-		if len(cfg.OutputPaths) == 1 {
-			return errors.New("no output file set")
-		}
-		cfg.OutputPaths = cfg.OutputPaths[1:]
-		return nil
-	}
-}
-
-// WithFields sets initial fields in the configuration
-func WithFields(fields map[string]interface{}) Option {
-	return func(cfg *zap.Config) error {
-		cfg.InitialFields = fields
-		return nil
-	}
-}
 
 // New creates a default zap logger based with basic defaults.
 // If `logpath` is set, will use json encoder and write to the provided filepath.
